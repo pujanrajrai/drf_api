a883603408f966037f3a78f9460711c5bb0efcd0
Mac <mac@Macs-MacBook-Pro.local>
2022-05-04 14:55:58 +0545

    add

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .serializers import StudentSerializers
 from .models import Student
 from rest_framework.generics import ListAPIView, CreateAPIView
@@ -9,6 +12,8 @@ from rest_framework.generics import ListAPIView, CreateAPIView
 class StudentListView(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializers
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
 
 class StudentCreateView(CreateAPIView):
